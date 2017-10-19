099546c4a4e3e2f38365341906f5f8635b15a1e3
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-19 23:58:19 +0300

    Added event listener for mouse click and keyboard press function, for input field

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-//Controller module
+//Budget Controller module
 
 var budgetController = (function () {
 
-    var x = 23;
 
-    var add = function (a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function (b) {
-            console.log(add(b));
-        }
-    }
 
 
 
@@ -38,15 +28,27 @@ var UIController = (function () {
 
 
 
-// App controller
+// Global App controller module
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function () {
-            console.log(z);
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        //1. Get the filed input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+    });
+
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+
         }
-    }
+    });
 
 })(budgetController, UIController);
