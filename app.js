1a3f2e4b63d87b874c3e1baa1ca4f6d59947fa76
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-24 23:57:55 +0300

    Created data module in BudgetController module

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
-
-
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
 
 
