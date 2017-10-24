c65eb99e7f389e798187fcc852e4f5bd75543ead
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-25 00:17:53 +0300

    Created function constructor for Income and Expense

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ var budgetController = (function () {
 
 
 
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
 })();
 
 
